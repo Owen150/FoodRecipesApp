3a114d4a1cec952e6a8397a752e063ffffb84ddb
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-02-22 21:35:49 +0300

    Dependencies Updates

--- app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.easyfood.fragments
 
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders.*
 import androidx.lifecycle.get
 import com.bumptech.glide.Glide
 import com.example.easyfood.R
@@ -30,7 +28,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        homeMvvm = androidx.lifecycle.ViewModelProviders.of(this)[HomeViewModel::class.java]
+        homeMvvm = ViewModelProvider(this).get(HomeViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -51,8 +49,10 @@ class HomeFragment : Fragment() {
 
     private fun observerRandomMeal() {
         homeMvvm.observeRandomMealLivedata().observe(viewLifecycleOwner,object : Observer<Meal>{
-            override fun onChanged(t: Meal?) {
-                TODO("Not yet implemented")
+            override fun onChanged(t: Meal?) {      //Will give you the meal which is stored in variable t
+                Glide.with(this@HomeFragment) //Listening to the Random meal live data variable and whenever it changes execute method
+                    .load(t!!.strMealThumb)
+                    .into(binding.imgRandomMeal)
             }
 
         })
