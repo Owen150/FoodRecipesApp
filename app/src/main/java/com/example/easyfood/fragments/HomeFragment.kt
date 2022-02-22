ea6f67df4b099ade9c1b00971c1138a249a164d4
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-02-22 13:27:40 +0300

    Retrofit Updates

--- app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.easyfood.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.easyfood.R
+import com.example.easyfood.pojo.Meal
 import com.example.easyfood.pojo.MealList
 import com.example.easyfood.retrofit.RetrofitInstance
 import retrofit2.Call
@@ -32,12 +34,19 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList>{
+            //Means that retrofit is connected to our API
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
-                TODO("Not yet implemented")
+            //Get the information of the random meal and show it in the random view
+                if (response.body() !=null){
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    Log.d("TEST", "meal id ${randomMeal.idMeal} name ${randomMeal.strMeal}")
+                }else{
+                    return
+                }
             }
-
+            //Connection to the API was unsuccessful - log the error message
             override fun onFailure(call: Call<MealList>, t: Throwable) {
-                TODO("Not yet implemented")
+                Log.d("HomeFragment", t.message.toString())
             }
         })
     }
