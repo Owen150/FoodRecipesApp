f1a52d287c3d45a2429c38a7fd2615daebe5428c
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-03-11 18:33:23 +0300

    Fixed the MealsViewModel

--- app/src/main/java/com/example/easyfood/viewModel/MealViewModel.kt
+++ app/src/main/java/com/example/easyfood/viewModel/MealViewModel.kt
@@ -4,19 +4,14 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.easyfood.db.MealDatabase
 import com.example.easyfood.pojo.Meal
 import com.example.easyfood.pojo.MealList
 import com.example.easyfood.retrofit.RetrofitInstance
-import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MealViewModel(
-    private val mealDatabase: MealDatabase
-):ViewModel() {
+class MealViewModel():ViewModel() {
     private var mealDetailsLiveData = MutableLiveData<Meal>()
 
     fun getMealDetail(id:String){
@@ -28,24 +23,12 @@ class MealViewModel(
             }
 
             override fun onFailure(call: Call<MealList>, t: Throwable) {        //Log the error message
-                    Log.d("MealActivity", t.message.toString())
-                }
+                Log.d("MealActivity", t.message.toString())
+            }
 
         })
     }
     fun observerMealDetailsLiveData():LiveData<Meal>{
         return mealDetailsLiveData
-    }
-
-    fun insertMeal(meal: Meal){
-        viewModelScope.launch {
-            mealDatabase.mealDao().upsert(meal)
-        }
-    }
-
-    fun deleteMeal(meal: Meal){
-        viewModelScope.launch {
-            mealDatabase.mealDao().delete(meal)
-        }
     }
 }
